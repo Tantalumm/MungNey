7b972e162a6e7c50d6fede840e27b2e149079e8a
tantalumm <tandemon45@gmail.com>
2024-02-02 14:03:39 +0700

    start_front

---
+++ client/src/components/layout/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({Children}) => {
+    return(
+        <>
+            <Header/>
+            <div className="content">
+                {Children}
+
+            </div>
+            <Footer/>
+            <div>
+
+            </div>
+
+        </>
+    );
+};
+
+export default Layout;
